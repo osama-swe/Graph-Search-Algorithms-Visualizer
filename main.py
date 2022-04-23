45eaf00a6a800b4f9e12931314f4756cd71b5b42
osama-swe <osamayman.swe@gmail.com>
2022-04-24 01:32:00 +0200

    DFS without animation

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-import networkx as nx
-import matplotlib.pyplot as plt
 
-G = nx.Graph()
-G.add_nodes_from(range(1, 11))
-G.add_edges_from([(1, 2), (5, 9), (5, 4), (5, 1), (10, 6), (5, 7), (10, 3), (10, 2), (8, 1)])
-plt.figure("Graph")
-color_map = []
-for i in range(0, G.number_of_nodes()):
-    if i == 2:
-        color_map.append('gold')
-    elif i == 4:
-        color_map.append('lightgreen')
-    else:
-        color_map.append('lightgray')
-nx.draw_networkx(G, node_color=color_map, pos=nx.spring_layout(G, iterations=100), arrows=False, with_labels=True)
-plt.show()
+def dfs(node, goals, G):
+    visited = []
+    stack = [node]
+    while stack:
+        node = stack.pop()
+
+        if node not in visited:
+            visited.append(node)
+            if str(node) in goals:
+                return visited
+        for neighbour in G[node]:
+            if not neighbour in visited:
+                stack.append(neighbour)
+
